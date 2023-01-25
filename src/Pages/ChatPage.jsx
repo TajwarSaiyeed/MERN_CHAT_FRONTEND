344b9e3b30e26f7acfa728707ecb3594c8d61b67
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-25 07:51:08 +0600

    chakraUI | Chat page | axios chats fetch

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  const fetchChats = async () => {
+    const { data } = await axios.get("/api/chat");
+    setChats(data);
+  };
+
+  return (
+    <div>
+      {chats?.map((chat) => (
+        <div key={chat._id}>{chat.chatName}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ChatPage;
