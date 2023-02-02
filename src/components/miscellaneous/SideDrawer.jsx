02c253f793e29c98010a2399e1976eed250708dd
TajwarSaiyeed <rjabid36@gmail.com>
2023-02-02 17:36:05 +0600

    side drawer

--- src/components/miscellaneous/SideDrawer.jsx
+++ src/components/miscellaneous/SideDrawer.jsx
@@ -1,5 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
+import { FcSearch } from "react-icons/fc";
+import { FaBell } from "react-icons/fa";
+import { BsChevronDown } from "react-icons/bs";
 
 const SideDrawer = () => {
   const [search, setSearch] = useState();
@@ -9,7 +20,53 @@ const SideDrawer = () => {
 
   return (
     <>
-      <Box></Box>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        bg={"white"}
+        w={"100%"}
+        p={"5px 10px 5px 10px"}
+        borderWidth={"5px"}
+      >
+        <Tooltip label="Search Users to Chat">
+          <Button variant={"ghost"}>
+            <FcSearch />
+            <Text
+              display={{
+                base: "none",
+                md: "flex",
+              }}
+              px={"4"}
+            >
+              Search User
+            </Text>
+          </Button>
+        </Tooltip>
+        <Text fontSize={`2xl`} fontFamily={`Work sans`} color={`black`}>
+          Talk-A-Tive
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <FaBell
+                fontSize={"2xl"}
+                style={{
+                  marginRight: "10px",
+                }}
+              />
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+
+          <Menu>
+            <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+              {/* <Avatar /> */}
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+        </div>
+      </Box>
     </>
   );
 };
